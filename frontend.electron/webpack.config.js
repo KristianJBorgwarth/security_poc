c542ddc6cae62b9a2faa9dd8e45c29521fc6bed1
Kristian <fich2402@gmail.com>
2024-10-17 18:33:12 +0200

    init electron app

---
+++ frontend.electron/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/react/index.tsx',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',  // Use ts-loader to handle TypeScript + JSX
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+		directory: path.join(__dirname, 'dist'),  // This replaces contentBase
+	  },
+	  hot: true,
+	  open: false,
+	  port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/react/index.html'
+    })
+  ]
+};
