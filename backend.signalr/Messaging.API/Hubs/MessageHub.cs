e31922fa7cb685cef4273e6abacf4c1faf87a03c
Kristian <fich2402@gmail.com>
2024-10-18 10:44:40 +0200

    init signalr server app

---
+++ backend.signalr/Messaging.API/Hubs/MessageHub.cs
@@ -0,0 +1,15 @@
+﻿using Messaging.API.Services;
+using Microsoft.AspNetCore.SignalR;
+using SignalRSwaggerGen.Attributes;
+
+namespace Messaging.API.Hubs;
+
+[SignalRHub]
+public class MessageHub(IMessageService messageService) : Hub
+{
+    [SignalRMethod]
+    public async Task SendMessageAsync(string user, string message)
+    {
+        await messageService.SendMessageAsync(user, message);
+    }
+}
---
+++ backend.signalr/Messaging.API/Services/MessageService.cs
@@ -0,0 +1,19 @@
+﻿using Messaging.API.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Messaging.API.Services;
+
+public class MessageService : IMessageService
+{
+    private readonly IHubContext<MessageHub> _hub;
+
+    public MessageService(IHubContext<MessageHub> hub)
+    {
+        _hub = hub;
+    }
+
+    public async Task SendMessageAsync(string user, string message)
+    {
+        await _hub.Clients.Client(user).SendAsync("ReceiveMessage", message);
+    }
+}
