fa812e1ff59f2e383f08b84dfa00d4943d658b91
Kristian <fich2402@gmail.com>
2024-10-18 12:57:23 +0200

    added icons to sidepanel

--- frontend.electron/src/react/components/SidePanel.tsx
+++ frontend.electron/src/react/components/SidePanel.tsx
@@ -1,17 +1,21 @@
 import React from "react";
-import { HomeIcon, UserIcon, CogIcon } from "@heroicons/react/24/outline";
+import { HomeIcon, UserIcon, Cog6ToothIcon, ChatBubbleOvalLeftIcon, UserGroupIcon } from "@heroicons/react/24/outline";
 
 export const SidePanel: React.FC = () => {
   return (
-    <div className="bg-gray-800 text-white w-[60px] h-full flex flex-col items-center py-4 space-y-8">
-      {/* Home Icon */}
-      <HomeIcon className="w-6 h-6 cursor-pointer hover:text-gray-300" />
+    <div className="bg-gray-800 text-white w-[60px] h-screen flex flex-col justify-between py-4">
+      <div className="flex flex-col items-center space-y-8">
+        {/* Home Icon */}
+        <HomeIcon className="w-6 h-6 cursor-pointer hover:text-gray-300" />
+        <ChatBubbleOvalLeftIcon className="w-6 h-6 cursor-pointer hover:text-gray-300" />
+        <UserGroupIcon className="w-6 h-6 cursor-pointer hover:text-gray-300" />
+      </div>
 
-      {/* User Profile Icon */}
-      <UserIcon className="w-6 h-6 cursor-pointer hover:text-gray-300" />
-
-      {/* Settings Icon */}
-      <CogIcon className="w-6 h-6 cursor-pointer hover:text-gray-300" />
+      {/* Bottom Icons: User Profile and Settings */}
+      <div className="flex flex-col items-center space-y-8">
+        <UserIcon className="w-6 h-6 cursor-pointer hover:text-gray-300" />
+        <Cog6ToothIcon className="w-6 h-6 cursor-pointer hover:text-gray-300" />
+      </div>
     </div>
   );
 };
