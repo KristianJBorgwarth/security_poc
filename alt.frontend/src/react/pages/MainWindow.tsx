3eb767f5f8d0aff0070957a3b732dc788697d475
Kristian <fich2402@gmail.com>
2024-10-26 18:12:53 +0200

    added tailwindcss and some components

---
+++ alt.frontend/src/react/pages/MainWindow.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { SidePanel } from "../components/SidePanel";
+import { ChatWindow } from "../components/chat/ChatWindow";
+
+export const MainWindow: React.FC = () => {
+  return (
+    <div className="flex h-screen w-screen">
+      {/* SidePanel */}
+      <SidePanel />
+
+      {/* Main content (ChatWindow) */}
+      <div className="flex-grow flex flex-col">
+        <ChatWindow />
+      </div>
+    </div>
+  );
+};
