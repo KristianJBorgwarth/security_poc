d4954d68ff93b92af53cf8437dc78d3950de936b
Kristian <fich2402@gmail.com>
2024-10-27 18:29:59 +0100

    ref to create user page

--- alt.frontend/src/react/App.tsx
+++ alt.frontend/src/react/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { MainWindow } from "./pages/MainWindow";
-import LoginPage from "./pages/LoginPage";
+import CreateUserPage from "./pages/LoginPage";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<CreateUserPage />} />
         <Route path="/main" element={<MainWindow />} />
       </Routes>
     </Router>
--- alt.frontend/src/react/pages/LoginPage.tsx
+++ alt.frontend/src/react/pages/LoginPage.tsx
@@ -3,41 +3,44 @@ import { useNavigate } from "react-router-dom";
 import InputBox from "../components/common/InputBox";
 import Button from "../components/common/Button";
 
-const LoginPage: React.FC = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+const CreateUserPage: React.FC = () => {
+  const [username, setUsername] = useState("");
+  const [connectionID, setConnectionId] = useState("");
   const navigate = useNavigate();
 
-  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+  const createUser = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log;
+    console.log("Creating user...");
+    window.electron
+      .createUser(username, connectionID)
+      .then(() => {
+        navigate("/main");
+      })
+      .catch(err => {
+        console.error(err);
+      });
   };
 
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-zinc-950 text-white">
       <div className="flex flex-col items-center w-full max-w-xs">
         <h1 className="text-2xl font-bold mb-6 text-center text-white">LOGIN</h1>
-        <form onSubmit={handleLogin} className="flex flex-col items-center w-full space-y-4">
-          <div className="w-full mb-4">
-            <InputBox
-              type="text"
-              placeholder="Enter your email"
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-            />
+        <form onSubmit={createUser} className="flex flex-col items-center w-full space-y-4">
+          <div className="w-full">
+            <InputBox type="text" placeholder="Usename" value={username} onChange={e => setUsername(e.target.value)} />
 
             <InputBox
-              type="password"
-              placeholder="Enter your password"
-              value={password}
-              onChange={e => setPassword(e.target.value)}
+              type="text"
+              placeholder="Connection ID (UUID)"
+              value={connectionID}
+              onChange={e => setConnectionId(e.target.value)}
             />
           </div>
+          <Button type="submit" text="Login" />
         </form>
-        <Button type="submit" text="Login" onClick={() => navigate("/main")} />
       </div>
     </div>
   );
 };
 
-export default LoginPage;
+export default CreateUserPage;
